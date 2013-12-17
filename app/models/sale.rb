a78aedf9a57e23d6857f496c6348dd054ed09ccd
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2013-12-17 07:01:38 -0500

    All pages displaying correctly.  Still need to sort by order and limit by sale date

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,10 +1,5 @@
 class Sale < ActiveRecord::Base
 
-  has_one :employee
-  has_one :customer
-  has_one :product
-  has_one :frequency
-
   belongs_to :employee
   belongs_to :customer
   belongs_to :product
